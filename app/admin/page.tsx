7e923cbabd1b2e59f7ef64b971be97c00834f646
Calvin Oker <calvinoker8@gmail.com>
2026-01-01 22:10:10 +0300

    God

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,8 @@
 
 import React, { useEffect, useState } from 'react'
 import { useAuth } from '../context/AuthContext'
-import { LogoutButton } from '../logout/page'
+import { LogoutButton } from '../components/LogoutButton';
+
 
 type SalaryRecord = {
   id: number
